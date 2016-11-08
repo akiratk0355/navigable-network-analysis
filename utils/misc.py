db1fcfe61b11220236ea42126dc8461081b85766
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2016-11-08 19:10:39 +0200

    Added helper funcs

--- utils/misc.py
+++ utils/misc.py
@@ -3,6 +3,10 @@ Created on Oct 27, 2016
 
 @author: akira
 '''
+import random
+
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class MiscException(Exception):
     pass
@@ -14,3 +18,31 @@ def dist_ring(source, target, size): # helper func
     
     return min(abs(source - target), size - abs(source - target))
     
+def localcon_ring(node, size):
+    if node == 0:
+        return (size-1, 1)
+    elif node == size-1:
+        return (size-2, 0)
+    else:
+        return (node-1, node+1)
+
+def shuffle_position_ring(G, iteration=None):
+    size = G.number_of_nodes()
+    if not iteration:
+        iteration = size
+    # store original position
+    for nd in G.nodes():
+        G.node[nd]['original'] = nd
+    
+    for _ in range(0, iteration):     
+        n1 = int(random.uniform(0, G.number_of_nodes()))
+        n2 = int(random.uniform(0, G.number_of_nodes()))
+        #print("switching %d and %d" % (n1, n2))
+        G = nx.relabel_nodes(G, {n1:n2,n2:n1})
+        
+    labels = dict((n,d['original']) for n,d in G.nodes(data=True))
+    
+    return (G, labels)
+        
+
+
