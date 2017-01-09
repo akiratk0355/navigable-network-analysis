c4fddbd4d283f638fbd85fcaf0b4f369514610d6
Akira Takahashi <ak.sm0poxid12ed@gmail.com>
2017-01-09 12:02:21 +0200

    Implemented D^2-DFS

--- utils/search.py
+++ utils/search.py
@@ -1,26 +1,46 @@
 import logging, random
-from math import sqrt
 
 import numpy as np
 
-from utils.misc import dist_lattice, id_dist_ring, id_localcon_ring
-
+from utils.misc import id_dist_ring, id_localcon_ring
 
 logger = logging.getLogger(__name__)
+
+SOURCE = -1
 
 class RoutingError(Exception):
     pass
 
-def greedy_path(G, source, target, dim=1, cutoff=None, use_local=False, strict=True, deg_dict={}):
+class Node(object):
+    def __init__(self, id):
+        self.id = id
+        self.visited = False
+        self.predecessor = None
+    
+    def add_predecessor(self, node):
+        self.predecessor = node
+    
+    def get_predecossor(self):
+        return self.predecessor
+    
+    def mark(self):
+        self.visited = True
+    
+    def is_visited(self):
+        return self.visited
+
+def greedy_path(G, source, target, dim=1, cutoff=None, 
+                use_local=False, strict=True, backtrack=False, deg_dict={}):
     path = [source]
     curr = source
     step = 0
     visited = {}
+    pred_dict = {source:SOURCE}
     
     while not curr == target:
         visited[curr] = True
         curr_to_tgt = id_dist_ring(curr, target)
-        logger.debug("current node is %d: d(%d, %d)=%d", curr, curr, target, curr_to_tgt)
+        logger.debug("current node is {0:.6f}: d({1:.6f}, {2:.6f})={3:.6f}".format(curr, curr, target, curr_to_tgt))
         if cutoff and step >= cutoff:
             raise RoutingError("number of hops reached a limit %d, terminating" % cutoff) 
         best = None
@@ -30,19 +50,28 @@ def greedy_path(G, source, target, dim=1, cutoff=None, use_local=False, strict=T
             if visited.get(neigh, False):
                 continue
             d = id_dist_ring(neigh, target)
-            logger.debug("checking neighbor %d: d(%d, %d)=%d", neigh, neigh, target, d)
+            logger.debug("checking neighbor {0:.6f}: d({1:.6f}, {2:.6f})={3:.6f}".format(neigh, neigh, target, d))
             if d/deg_dict.get(neigh,1.0) <= best_to_tgt:
                 best = neigh
                 best_to_tgt = d/deg_dict.get(neigh,1.0)                
         
-        if curr_to_tgt < best_to_tgt:
-            logger.debug("encountered dead-end at %d !", curr)
+        if curr_to_tgt >= best_to_tgt:
+            pred_dict[best] = curr
+        else:
+            logger.debug("encountered dead-end at %f !", curr)
             if not strict:
                 if best == None: # no unvisited neighbors
-                    logger.debug("node %d has no unvisited neighbors", curr)
-                    raise RoutingError("terminating at dead-end node %d: no unvisited neighbors" % curr)
+                    if backtrack and pred_dict[curr] == SOURCE:
+                        raise RoutingError("terminating at dead-end node %f: source has touched all the neighbors" % curr)
+                    elif backtrack:
+                        best = pred_dict[curr]
+                        logger.debug("node %f has no unvisited neighbors, returning to its predecessor %f", curr, best)
+                    else:
+                        logger.debug("node %f has no unvisited neighbors", curr)
+                        raise RoutingError("terminating at dead-end node %f: no unvisited neighbors" % curr)
                 else:
-                    logger.debug("using the second best node %d", best)
+                    pred_dict[best] = curr
+                    logger.debug("using the second best node %f", best)
             elif use_local:
                 ndlist = sorted(G.nodes())
                 pre, suc = id_localcon_ring(ndlist, curr)
@@ -50,9 +79,10 @@ def greedy_path(G, source, target, dim=1, cutoff=None, use_local=False, strict=T
                     best = pre
                 else:
                     best = suc
-                logger.debug("using local connection %d", best)
+                pred_dict[best] = curr
+                logger.debug("using local connection %f", best)
             else:
-                raise RoutingError("terminating at dead-end node %d: no nodes closer to the target" % curr) 
+                raise RoutingError("terminating at dead-end node %f: no nodes closer to the target" % curr) 
                 
         # move on to next node
         step += 1
@@ -60,10 +90,12 @@ def greedy_path(G, source, target, dim=1, cutoff=None, use_local=False, strict=T
         curr = best
         
     logger.debug("routing succeeded after %d steps!!", step)
+    logger.debug("pred_dict=%s", pred_dict)
     visited[target] = True
     return (path, visited, step)
 
-def average_greedy_path_length(G, trial_per_src=5, debug=False, cutoff=None, use_local=False, strict=True, use_deg=False):
+def average_greedy_path_length(G, trial_per_src=5, debug=False, cutoff=None, 
+                               use_local=False, strict=True, use_deg=False, backtrack=False):
     lsum = 0
     success = 0
     size = G.number_of_nodes()
@@ -87,7 +119,8 @@ def average_greedy_path_length(G, trial_per_src=5, debug=False, cutoff=None, use
             
             #print("finding path from %s to %s" % (src, dst))
             try:
-                path, vstd, step = greedy_path(G, src, dst, cutoff=cutoff, use_local=use_local, strict=strict, deg_dict=deg_dict)
+                path, vstd, step = greedy_path(G, src, dst, cutoff=cutoff,
+                                               use_local=use_local, strict=strict, deg_dict=deg_dict, backtrack=backtrack)
             except RoutingError:
                 #print("failed")
                 continue
